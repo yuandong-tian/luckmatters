ebc5e05bcc9bad5ccb1204a87116bb8b2bf3074b
Yuandong Tian <yuandong.tian@gmail.com>
2020-02-28 21:39:47 -0800

    Choose not to use data_aug for cifar

--- catalyst/dataset.py
+++ catalyst/dataset.py
@@ -55,12 +55,18 @@ def init_dataset(args):
         transforms.Normalize(
             (0.5,), (0.5,))]) 
 
-    transform_cifar10_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
+    if args.use_data_aug: 
+        transform_cifar10_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+    else:
+        transform_cifar10_train = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
 
     transform_cifar10_test = transforms.Compose([
         transforms.ToTensor(),
